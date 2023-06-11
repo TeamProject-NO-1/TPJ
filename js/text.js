1014e417a0d60c57f0a5f8e99c0690261401e835
Yabuinu98 <k4uk4u@naver.com>
2023-06-11 17:45:01 +0900

    tpj

---
+++ js/text.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const titleInput = document.querySelector("title_input")
+    const titleText = document.querySelector("title_text")
+    const titleName = document.querySelector("title_name")
+
+    /* localStorage 부분 */
+    // 구분 숫자메기는 건 forEach 인덱스로 붙이고
+    // 제목, 작성자, 작성일자를 객체로 저장해서 list.html에서 쓴다
+})
