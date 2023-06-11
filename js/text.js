8a567ed415993951bd2728d34936e9f7c9d6ffba
Yabuinu98 <k4uk4u@naver.com>
2023-06-11 21:18:39 +0900

    tpj

--- js/text.js
+++ js/text.js
@@ -6,4 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     /* localStorage 부분 */
     // 구분 숫자메기는 건 forEach 인덱스로 붙이고
     // 제목, 작성자, 작성일자를 객체로 저장해서 list.html에서 쓴다
+    let saveData = localStorage.getItem("arr")
+    titleInput.addEventListener("keyup", () => {
+    })
 })
