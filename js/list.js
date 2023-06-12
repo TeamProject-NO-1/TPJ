105d5183ec0fb284a4a5e40150b662d654951b30
Yabuinu98 <k4uk4u@naver.com>
2023-06-12 15:47:41 +0900

    tpj

--- js/list.js
+++ js/list.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const Table = document.querySelector(".Table_Area")
+    /* localstorage data를 추가할 곳 */
     const table = document.createElement("table")
+    /* 생성하기 */
     const tr = document.createElement("tr")
-    const td1 = document.createElement("td")
-    const td2 = document.createElement("td")
-    const td3 = document.createElement("td")
-    const td4 = document.createElement("td")
-    const td5 = document.createElement("td")
-    Table.appendChild(table).appendChild(tr).append(td1, td2, td3, td4, td5)
-    td1.textContent = "글번호"
-    td2.textContent = "제목"
-    td3.textContent = "작성자"
-    td4.textContent = "조회수"
-    td5.textContent = "작성일자"
+    let td1 = document.createElement("td")
+    let td2 = document.createElement("td")
+    let td3 = document.createElement("td")
+    let td4 = document.createElement("td")
+    let td5 = document.createElement("td")
+
+    /* localstorage를 바디에 뿌려주기 */
+    let array = localStorage.getItem("input")
+    console.log(array)
+    array.forEach((v) => {
+    table.appendChild(tr).append(td1, td2, td3, td4, td5)
+    })
+    td1.textContent = 1
+    td2.textContent = v.제목
+    td3.textContent = v.작성자
+    td5.textContent = v.작성일자
 })
--- js/text.js
+++ js/text.js
@@ -5,21 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const 등록버튼 = document.getElementById("update")
 
     /* localStorage 부분 */
-    // 구분 숫자메기는 건 forEach 인덱스로 붙이고
-    // 제목, 작성자, 작성일자를 객체로 저장해서 list.html에서 쓴다
+    // 순번, 제목, 작성자, 작성일자를 객체로 저장해서 list.html에서 쓴다
 
-    /* arr에 localStroage input속성에 값이 있으면 빈배열 출력 */
+    /* 1. arr에 localStroage input속성에 값이 있으면 빈배열 출력 */
+    // 아래 구문에서 푸쉬할 예정 => 제일 상단에 있지만 제일 마지막 느낌임
     let arr = JSON.parse(localStorage.getItem('input')) ?? []
+    
+    /* 2. 순번이 저장된 변수 */
+    let ct = 0
 
     /* 등록버튼을 누르면 -> localStroage에 저장되게하는 부분 */
     등록버튼.addEventListener('click', () => {
-      // 버튼을 누른 날짜를 받아오기 위해서
-      // new Date() 사용 => 'today.get~()'를 사용하기 위해
+      /* 날짜를 받아오기 위해서 */
+      /* new Date() 사용 => 'today.get~()'를 사용하기 위해 */
       let today = new Date();
-      let year = today.getFullYear(); // 년도
+      let year = today.getFullYear();
       let month = today.getMonth() + 1;  // 월(0~11까지 돌아서 +1)
-      let date = today.getDate();  // 날짜
+      let date = today.getDate();
 
+      /* 값을 입력 하지 않으면 alert로 경고문구 띄우기 */
       if (titleInput.value == "") {
         alert('제목을 입력해주세요.');
         titleInput.focus();
@@ -30,15 +34,20 @@ document.addEventListener("DOMContentLoaded", () => {
         alert('작성자를 입력해주세요.');
         titleName.focus();
       } else {
-        /* arr배열에 객체 형식으로 밀어줄텐데 */
+        /* 2-1. 등록버튼이 누릴때마다 ct가 1씩 증가(list.html의 글번호) */
+        ct++
+        /* 1-1. arr배열에 객체 형식으로 밀어주기 */
         arr.push({ 
+          순번: ct,
           제목: titleInput.value, 
           내용: titleText.value, 
           작성자: titleName.value, 
           작성일자: year + '/' + month + '/' + date })
+
+          /* 1-2. 앞서 push받은 자료들을 localStorage input속성에 저장 */
           localStorage.setItem('input', JSON.stringify(arr))
   
-        /* input태그, textarea태그 내용 비워주기 */
+        /* 1-3. 저장 후 input, textarea태그 내용 비우기(잔존 데이터 제거) */
         titleInput.value = '';
         titleText.value = '';
         titleName.value = '';
